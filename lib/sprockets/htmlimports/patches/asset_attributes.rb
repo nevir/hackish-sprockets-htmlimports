364f9e88aecb31f1b9bf7b35263dc3dacd6dc41b
Ian MacLeod <imac@google.com>
2014-06-15 10:25:17 -0700

    Fix some pathing issues.

--- lib/sprockets/htmlimports/patches/asset_attributes.rb
+++ lib/sprockets/htmlimports/patches/asset_attributes.rb
@@ -1,7 +1,7 @@
 require 'sprockets/asset_attributes'
 
 Sprockets::AssetAttributes.class_eval do
-  IMPLICIT_WRAP_EXTENSION = '.__IMPLICIT_WRAP:__'
+  IMPLICIT_WRAP_EXTENSION = '.WRAP_IN:'
 
   def self.wrap_for_format(path, format_extension)
     Pathname.new(path.to_s + IMPLICIT_WRAP_EXTENSION + format_extension)
@@ -22,5 +22,10 @@ Sprockets::AssetAttributes.class_eval do
   def real_logical_path
     return logical_path unless implicitly_wrapped?
     logical_path[0...-implicit_extensions.map(&:size).reduce(:+)]
+  end
+
+  def real_pathname
+    return pathname unless implicitly_wrapped?
+    Pathname.new(pathname.to_s[0...-implicit_extensions.map(&:size).reduce(:+)])
   end
 end
--- lib/sprockets/htmlimports/patches/base.rb
+++ lib/sprockets/htmlimports/patches/base.rb
@@ -3,14 +3,22 @@ require 'sprockets/htmlimports/wrapped_asset'
 
 Sprockets::Base.class_eval do
   def find_asset_with_implicit_wrapping(path, options = {})
-    puts "find_asset(#{path.inspect}, #{options.inspect})"
     attributes = attributes_for(path)
     unless attributes.implicitly_wrapped?
       return find_asset_without_implicit_wrapping(path, options)
     end
 
-    real_path = attributes.real_logical_path
-    asset = find_asset_without_implicit_wrapping(real_path, options)
+    # Make sure that we are dealing with a fully resolved path.
+    unless attributes.pathname.absolute?
+      real_pathname = resolve(attributes.real_pathname)
+      attributes = attributes_for(Sprockets::AssetAttributes.wrap_for_format(
+        real_pathname, attributes.implicit_format_extension
+      ))
+    end
+
+    real_logical_path = attributes.real_logical_path
+    asset = find_asset_without_implicit_wrapping(real_logical_path, options)
+
     Sprockets::HTMLImports::WrappedAsset.new(self, attributes, asset)
   end
   alias_method :find_asset_without_implicit_wrapping, :find_asset
